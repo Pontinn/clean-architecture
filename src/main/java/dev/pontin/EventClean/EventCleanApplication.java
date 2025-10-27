dc508c812a2d6e44c59fb1adfd202d45cdff6261
Pontinn <leo.pontin2@gmail.com>
2025-10-27 08:25:21 -0300

    first commit

---
+++ src/main/java/dev/pontin/EventClean/EventCleanApplication.java
@@ -0,0 +1,12 @@
+package dev.pontin.EventClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCleanApplication.class, args);
+	}
+}
